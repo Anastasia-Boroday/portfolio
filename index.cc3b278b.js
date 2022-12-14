8970656e63da1dcc433476eb54c58a45382c63bf
Anastasia-Boroday <93268396+Anastasia-Boroday@users.noreply.github.com>
2022-12-14 01:06:04 +0000

    Deploying to gh-pages from @ Anastasia-Boroday/portfolio@8b656cf940f404d345bfa91a7204b30ac931f2ad 🚀

---
+++ index.cc3b278b.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".btn-up");window.addEventListener("scroll",(function(){(window.scrollY||document.documentElement.scrollTop)>400?o.classList.remove("is-hidden"):o.classList.add("is-hidden")})),o.addEventListener("click",(function(){window.scrollTo({top:0,right:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.cc3b278b.js.map
---
+++ index.f577bf16.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".btn-up");window.addEventListener("scroll",(()=>{(window.scrollY||document.documentElement.scrollTop)>400?e.classList.remove("is-hidden"):e.classList.add("is-hidden")})),e.addEventListener("click",(()=>{window.scrollTo({top:0,right:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.f577bf16.js.map
